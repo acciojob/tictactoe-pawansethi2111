fbeb5d2b922664cf06496946e8b7c929707efd51
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-01-04 16:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,22 +11,22 @@ let chance  = 0;
 let message = document.getElementsByClassName('message');
  let player1 = user1.value;
 let player2  = user2.value;
-let arr  = new Array().fill("")
-let correctval = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+// let arr  = new Array().fill("")
+// let correctval = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 for(let i=0;i<tick.length;i++){ 
 tick[i].addEventListener("click",function(e){	  
 if(chance===0){	 
-	boardpage.innerHTML= `${user1.value}! You're up`;
-	tick[i].innerText = 'O';
+	boardpage.innerHTML= `${player1}! You're up`;
+	tick[i].innerText = 'O'; 
 	chance=1;
-	arr[.e.target.id-1] = "o"
+	//arr[.e.target.id-1] = "o"
 } 
 else{
-	boardpage.innerHTML = `${user2.value}! You're up`;
+	boardpage.innerHTML = `${player2}! You're up`;
 	tick[i].innerText ='X';
 	chance = 0;	
-		arr[.e.target.id-1] = "x"
+		//arr[.e.target.id-1] = "x"
 
 }
 });
@@ -34,6 +34,6 @@ else{
 
 
 
-function checkWinner(){
-	if()
-}
+// function checkWinner(){
+// 	if()
+// }
