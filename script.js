a59ff9c66debffac17554b3dbfb3aabdb7fe5887
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-01-12 13:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,27 +19,31 @@ let user2 = document.getElementById('player2');
 tick[i].addEventListener("click",function(e){	
 let player1 = user1.value;
 let player2  = user2.value;
+
 if(chance===0){	 
 	boardpage.innerHTML= `${player1}! You're up`;
 	tick[i].innerText = 'O'; 
 	chance=1;
-	//checkWinner(arr);
+	if(checkWinner(arr,player1)){
+		break;
+	}
 	//arr[.e.target.id-1] = "o"
 } 
 else{
 	boardpage.innerHTML = `${player2}! You're up`;
 	tick[i].innerText ='X';
 	chance = 0;	
-	//checkWinner(arr);
+	if(checkWinner(arr,player2)){
+		break;
+	}
 		//arr[.e.target.id-1] = "x"
-
 }
 });
 }
 
 
 
-function checkWinner(arr){	
+function checkWinner(arr,player){	
 if(arr[0] == arr[1] && arr[1] == arr[2])
 	||(arr[3]==arr[4]&& arr[4]==arr[5])
 	||(arr[6]==arr[7]&& arr[7]==arr[8])
@@ -49,10 +53,9 @@ if(arr[0] == arr[1] && arr[1] == arr[2])
 	||(arr[0]==arr[4]&& arr[4]==arr[8])
 	||(arr[2]==arr[4]&& arr[4]==arr[6])
 	{
-		
+		message.innerHTML = `${player} has won`;
 	}
-
-
+	return true;
 }
 
 
